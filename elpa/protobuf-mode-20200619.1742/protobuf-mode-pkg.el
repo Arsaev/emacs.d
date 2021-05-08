c2ae787551a0db551a265643fd16a883dcfd16b1
Magomed Arsaev <mago@hafven.de>
2021-05-08 16:54:24 +0200

    install vue-mode

---
+++ elpa/protobuf-mode-20200619.1742/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/mago/.emacs.d/elpa/protobuf-mode-20200619.1742/protobuf-mode.el  -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "20200619.1742" "major mode for editing protocol buffers." 'nil :commit "7e3bbeda920ccc1eb6942ec581a48ee6687a1cc7" :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com") :keywords '("google" "protobuf" "languages"))
--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(guru-mode auto-complete ## go-projectile projectile go-mode go-autocomplete exec-path-from-shell)))
+   '(vue-mode protobuf-mode guru-mode auto-complete ## go-projectile projectile go-mode go-autocomplete exec-path-from-shell)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
